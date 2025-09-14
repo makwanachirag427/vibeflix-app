b8ab4830117b87fef866fa5d138f4351c11ccabc
Chirag Makwana <makwanachirag427@gmail.com>
2025-09-14 19:19:13 +0530

    cookie issue resolved

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,11 +4,12 @@ import { ENV_VARS } from "../config/envVars.js";
 export const generateTokenAndSetCookie = (userId, res) => {
   const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: "15d" });
 
+  const isProd = ENV_VARS.NODE_ENV === "production";
   res.cookie("jwt", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
-    sameSite: "strict",
+    sameSite: isProd ? "none" : "lax",
     httpOnly: true,
-    secure: ENV_VARS.NODE_ENV !== "development",
+    secure: isProd,
   });
 
   return token;
